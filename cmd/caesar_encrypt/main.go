f93ba6b7069e13404759c9af3e3652dd73d7fc63
rocco barbini <roccobarbi@gmail.com>
2021-08-23 22:08:00 +0200

    Validation structure laid out

--- cmd/caesar_encrypt/main.go
+++ cmd/caesar_encrypt/main.go
@@ -58,6 +58,26 @@ func ingestArgs(args []string) [][]string {
 	return parsedArgs
 }
 
+func validateFlagF(configuration config, arg []string) (config, error) {
+	return configuration, errors.New("Not implemented.")
+}
+
+func validateFlagC(configuration config, arg []string) (config, error) {
+	return configuration, errors.New("Not implemented.")
+}
+
+func validateFlagP(configuration config, arg []string) (config, error) {
+	return configuration, errors.New("Not implemented.")
+}
+
+func validateFlagM(configuration config, arg []string) (config, error) {
+	return configuration, errors.New("Not implemented.")
+}
+
+func validateFlagO(configuration config, arg []string) (config, error) {
+	return configuration, errors.New("Not implemented.")
+}
+
 /**
 validateConfig parses the command line arguments and returns a configuration structure. The command line arguments are
 not read directly, but rather received as validateConfig's only argument (a string slice). validateConfig returns a
@@ -65,8 +85,31 @@ tuple with the config structure and an error object (or nil).
 */
 func validateConfig(args []string) (config, error) {
 	var configuration config
+	var err error
 	configuration.init()
 	parsedArgs := ingestArgs(args)
+	for _, arg := range parsedArgs {
+		if len(arg[0]) > 1 {
+			// Multiple flags have been grouped together, which may be acceptable to other programs, but in this case is
+			// an error (all arguments require an additional value).
+			return configuration, errors.New("Grouped flags found, but they are not supported by this program: -" + arg[0])
+		}
+		switch arg[0] {
+		case "f":
+			configuration, err = validateFlagF(configuration, arg)
+		case "c":
+			configuration, err = validateFlagC(configuration, arg)
+		case "p":
+			configuration, err = validateFlagP(configuration, arg)
+		case "m":
+			configuration, err = validateFlagM(configuration, arg)
+		case "o":
+			configuration, err = validateFlagO(configuration, arg)
+		}
+		if err != nil {
+			return configuration, err
+		}
+	}
 	return configuration, nil
 }
 
